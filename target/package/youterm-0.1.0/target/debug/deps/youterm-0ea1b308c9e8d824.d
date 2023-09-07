216544ac04f956488356271fe368c84a06971ea8
Hobo Spaceship <hobdenw@gmail.com>
2023-09-07 19:24:29 +0200

    first commit

---
+++ target/package/youterm-0.1.0/target/debug/deps/youterm-0ea1b308c9e8d824.d
@@ -0,0 +1,5 @@
+/home/hobo/Documents/dev/youterm/target/package/youterm-0.1.0/target/debug/deps/youterm-0ea1b308c9e8d824: src/main.rs
+
+/home/hobo/Documents/dev/youterm/target/package/youterm-0.1.0/target/debug/deps/youterm-0ea1b308c9e8d824.d: src/main.rs
+
+src/main.rs:
---
+++ target/package/youterm-0.1.0/target/debug/youterm.d
@@ -0,0 +1,1 @@
+/home/hobo/Documents/dev/youterm/target/package/youterm-0.1.0/target/debug/youterm: /home/hobo/Documents/dev/youterm/target/package/youterm-0.1.0/src/main.rs
